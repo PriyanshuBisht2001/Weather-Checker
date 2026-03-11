24bf65c0c4ae1e9b5bc409a2375a512373016811
priyanshugenshin <priyanshubisht159@gmail.com>
2026-03-11 14:36:40 +0530

    Refactor weather app: update API key handling, enhance HTML structure, and add dotenv dependency

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-
-const express = require('express');
-const { STATUS_CODES } = require('http');
-const https = require('https');
-const { removeData } = require('jquery');
-const bodyParser = require('body-parser')
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',function (req,res) {
-    res.sendFile(__dirname+"/index.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post("/",function (req,res) {
+app.post("/", function (req, res) {
+  const appId = process.env.OPENWEATHER_API_KEY;
+  const query = req.body.cityName;
+  const units = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    appId +
+    "&units=" +
+    units;
 
-    const appId="afe672b4a3696023c59632675013c069"
-    const query = req.body.cityName
-    const units="metric"
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+appId+"&units="+units
-    https.get(url,function(response){
-        console.log(response.statusCode);
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescrpition = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.json({
+        city: query,
+        temp: temp,
+        description: weatherDescrpition,
+        icon: imageURL,
+      });
+    });
+  });
+});
 
-        response.on("data",function(data){
-            const weatherData=JSON.parse(data);
-            const temp=weatherData.main.temp;
-            const weatherDescrpition = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
-            res.write("<h1>The temp in "+query+" is "+temp+" degree celcius </h1>")
-            res.write("<h1> Weather Description is "+weatherDescrpition+"</h1>")
-            res.write("<img src="+imageURL+">");
-            res.send()
-        })
-    })
-})
-
-app.listen(3000,function () {
-    console.log('This is listing to port 3000');
-})
+app.listen(3000, function () {
+  console.log("This is listing to port 3000");
+});
